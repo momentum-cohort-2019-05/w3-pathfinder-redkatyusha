ae6770c61e0c94b607a665d35dd49c9ca4a28294
Malyutka Kobanova <tankettedawn@gmail.com>
2019-06-15 14:45:16 -0400

    haven't committed in a while sorry

---
+++ clinton.py
@@ -0,0 +1,69 @@
+from PIL import Image
+
+
+def read_line_of_ints(text):
+    ints = []
+    ints_as_strs = split_line(text)
+
+    for int_as_str in ints_as_strs:
+        ints.append(int(int_as_str))
+    return ints
+
+
+def split_line(line):
+    return line.split()
+
+
+def read_file_into_list(filename):
+    with open(filename) as file:
+        return file.readlines()
+
+
+def read_file_into_ints(filename):
+    lines = read_file_into_list(filename)
+
+    list_of_lists = []
+    for line in lines:
+        list_of_lists.append(read_line_of_ints(line))
+    return list_of_lists
+
+
+class ElevationMap:
+    def __init__(self, elevations):
+        self.elevations = elevations
+
+    def elevation_at_coordinate(self, x, y):
+        return self.elevations[y][x]
+
+    def min_elevation(self):
+        return min([min(row) for row in self.elevations])
+
+    def max_elevation(self):
+        return max([max(row) for row in self.elevations])
+
+    def intensity_at_coordinate(self, x, y):
+        elevation = self.elevation_at_coordinate(x, y)
+        min_elevation = self.min_elevation()
+        max_elevation = self.max_elevation()
+
+        return (elevation - min_elevation) / (max_elevation - min_elevation)
+
+
+def draw_grayscale_gradient(filename, width, height):
+    image = Image.new(mode='L', size=(width, height))
+    for x in range(width):
+        for y in range(height):
+            image.putpixel((x, y), (int(x / width * 255),))
+    image.save(filename)
+
+
+if __name__ == "__main__":
+    my_str = "10 12 9 345 2 78"
+    read_line_of_ints(my_str)
+
+    elevations = read_file_into_ints('elevation_test.txt')
+
+    e_map = ElevationMap(elevations)
+    print(e_map.intensity_at_coordinate(1, 2))
+
+    draw_grayscale_gradient('test.png', 400, 400)
